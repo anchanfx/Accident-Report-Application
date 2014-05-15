b609c29b09ee5b647ded99816fc085559fe1ab22
watcharakit <gun_1288@hotmail.com>
2014-05-15 22:18:12 +0700

    delete GUNs' comment

--- AccidentReport/src/nu/ac/th/accidentreport/MainActivity.java
+++ AccidentReport/src/nu/ac/th/accidentreport/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends Activity {
 	
 	private TextView mTextView;
 	
-	// Test Comment by GunZ
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
